012041a301d1ba84dc0da63820e10d80cbf5c3bb
Aryan Mehta <aryanmehta2016@gmail.com>
2024-02-23 00:37:28 +0000

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/{22-february-portfolio}/",
+  base: "/22-february-portfolio/",
 });

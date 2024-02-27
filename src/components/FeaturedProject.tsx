a40e6ab0e1e0965070f178ce0063b9eed1765bff
Aryan Mehta <aryanmehta2016@gmail.com>
2024-02-27 01:13:38 +0000

    animating

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -4,8 +4,8 @@ import KanbanDesktopImage from "../assets/kanban-desktop.png";
 import { KeyboardArrowDown } from "@mui/icons-material";
 function FeaturedProject() {
   return (
-    <div className="bg-gradient-to-tr from-light-blue via-purple to-red p-8 lg:rounded-lg shadow-lg mt-64">
-      <div className="h-96 w-full flex flex-col gap-6 relative">
+    <div className="bg-gradient-to-tr from-light-blue via-purple to-red p-8 lg:rounded-lg shadow-lg mt-64 relative max-lg:py-32">
+      <div className="h-96 w-full flex flex-col gap-6 relative ">
         <span className="text-special uppercase opacity-70 relative z-50">
           featured
         </span>
@@ -24,22 +24,22 @@ function FeaturedProject() {
           <span>Jump to Project</span>
           <KeyboardArrowDown />
         </div>
-        <div className="absolute bottom-0 right-0 z-10 max-lg:hidden">
-          <MobileScreen
-            className="h-[26rem] w-52 -skew-x-2 skew-y-12 shadow-lg"
-            isMobile={true}
-          >
-            <img src={KanbanboardMobileImage} alt="" className="bg-cover" />
-          </MobileScreen>
-        </div>
-        <div className="absolute -top-40 -right-96 lg:right-0">
-          <MobileScreen
-            className="w-[32rem] lg:h-[26rem] lg:w-[40rem] lg:skew-x-2 lg:-skew-y-12 shadow-lg"
-            isMobile={false}
-          >
-            <img src={KanbanDesktopImage} alt="" />
-          </MobileScreen>
-        </div>
+      </div>
+      <div className="absolute bottom-0 right-0 z-10 max-lg:hidden">
+        <MobileScreen
+          className="h-[26rem] w-52 -skew-x-2 skew-y-12 shadow-lg"
+          isMobile={true}
+        >
+          <img src={KanbanboardMobileImage} alt="" className="bg-cover" />
+        </MobileScreen>
+      </div>
+      <div className="absolute -top-40 left-1/2 lg:-top-40  lg:right-0">
+        <MobileScreen
+          className="w-[32rem] lg:h-[26rem] lg:w-[40rem] lg:skew-x-2 lg:-skew-y-12 shadow-lg"
+          isMobile={false}
+        >
+          <img src={KanbanDesktopImage} alt="" />
+        </MobileScreen>
       </div>
     </div>
   );
